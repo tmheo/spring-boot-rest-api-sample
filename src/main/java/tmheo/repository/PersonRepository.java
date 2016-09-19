59b58b22c8383ebb9d34b93f301387447f0ddf92
taemyung <tmheo74@gmail.com>
2016-09-20 08:04:38 +0900

    - fix querydsl dependency

--- src/main/java/tmheo/repository/PersonRepository.java
+++ src/main/java/tmheo/repository/PersonRepository.java
@@ -1,11 +1,12 @@
 package tmheo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QueryDslPredicateExecutor;
 import tmheo.entity.Person;
 import tmheo.repository.custom.PersonRepositoryCustom;
 
 /**
  * Created by taemyung on 2016. 9. 10..
  */
-public interface PersonRepository extends JpaRepository<Person, Long>, PersonRepositoryCustom {
+public interface PersonRepository extends JpaRepository<Person, Long>, QueryDslPredicateExecutor<Person>, PersonRepositoryCustom {
 }
